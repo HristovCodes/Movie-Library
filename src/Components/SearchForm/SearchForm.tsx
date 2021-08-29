8ee1904fcafb57eee14cb544068f579b0ca44fd4
Hristov_Codes <ivailohristov1990@gmail.com>
2021-08-29 19:04:49 +0300

    Simple test for searching

--- src/Components/SearchForm/SearchForm.tsx
+++ src/Components/SearchForm/SearchForm.tsx
@@ -1,14 +1,30 @@
-import React, { FormEventHandler } from 'react';
-import './SearchForm.scss';
+import React, { FormEventHandler, useState } from "react";
+import "./SearchForm.scss";
 
-export default function SearchForm () {
-    const handleSubmit:FormEventHandler = (e)=> {
-        e.preventDefault();
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <input className="search"  placeholder="Search by movie title..." type="search"></input>
-            <button className="btnsubmit" type="submit">Search</button>
-        </form>
-    )
+export default function SearchForm({ setMovies }: any) {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${search}`
+    );
+
+    setMovies(await response.json());
+  };
+
+  return (
+    <form aria-label="form" onSubmit={handleSubmit}>
+      <input
+        onChange={(e) => setSearch(e.target.value)}
+        className="search"
+        placeholder="Search by movie title..."
+        type="search"
+      ></input>
+      <button className="btnsubmit" type="submit">
+        Search
+      </button>
+    </form>
+  );
 }

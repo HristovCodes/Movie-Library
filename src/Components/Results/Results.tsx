5559e7c414f73a325ae379de0b8af662a6642dc8
Hristov_Codes <ivailohristov1990@gmail.com>
2021-08-31 20:37:55 +0300

    Some styling to the search page

--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -9,9 +9,27 @@ interface ResultsProps {
 export default function Results({ movies }: ResultsProps) {
   return (
     <section className="Results">
-      <p>Results:</p>
+      <h1 className="Heading">Results:</h1>
       {movies.map((m: any) => {
-        return <div key={m.show.id}>{m.show.name}</div>;
+        const customstyles: React.CSSProperties = {
+          backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65) ), url(${m.show.image?.original})`,
+        };
+        return (
+          <div style={customstyles} className="Card" key={m.show.id}>
+            <h2>{m.show.name}</h2>
+            <p>
+              Category: <span>{m.show.type}</span>
+            </p>
+            <article
+              dangerouslySetInnerHTML={{ __html: m.show.summary }}
+            ></article>
+            <p>
+              <a rel="noreferrer" target="_blank" href={m.show.url}>
+                {m.show.url}
+              </a>
+            </p>
+          </div>
+        );
       })}
     </section>
   );

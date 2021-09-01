9019ab006c30076d138df8c3ed9d941a7b1a322e
Hristov_Codes <ivailohristov1990@gmail.com>
2021-09-01 16:02:38 +0300

    Styling and bugfixes

--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -1,33 +1,56 @@
 import React from "react";
-import { RootState } from "../../store";
+import { Link } from "react-router-dom";
+import { AppDispatch, RootState } from "../../store";
+import { useAppDispatch } from "../../hooks";
 import "./Results.scss";
+import placeholderimage from "../../media/placeholder.png";
 
 interface ResultsProps {
   movies: RootState;
 }
 
 export default function Results({ movies }: ResultsProps) {
+  const dispatch: AppDispatch = useAppDispatch();
+  const setMovies = (mvs: RootState) => {
+    dispatch({ type: "UPDATE_MOVIES", payload: mvs });
+  };
+
   return (
     <section className="Results">
       <h1 className="Heading">Results:</h1>
       {movies.map((m: any) => {
+        const full = m.show.summary ? m.show.summary : "";
+        const summ =
+          full.indexOf(".") <= 150
+            ? full.substring(0, full.indexOf(".") + 1)
+            : full.substr(0, 150) + "...";
+
         const customstyles: React.CSSProperties = {
-          backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65) ), url(${m.show.image?.original})`,
+          backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75) ), url(${
+            m.show.image?.original ? m.show.image?.original : placeholderimage
+          })`,
         };
         return (
           <div style={customstyles} className="Card" key={m.show.id}>
-            <h2>{m.show.name}</h2>
+            <Link
+              className="Name"
+              onClick={() => setMovies([m])}
+              to={`/movieinfo/${m.show.name}`}
+            >
+              {m.show.name}
+            </Link>
             <p>
               Category: <span>{m.show.type}</span>
             </p>
-            <article
-              dangerouslySetInnerHTML={{ __html: m.show.summary }}
-            ></article>
-            <p>
-              <a rel="noreferrer" target="_blank" href={m.show.url}>
-                {m.show.url}
-              </a>
-            </p>
+            <article dangerouslySetInnerHTML={{ __html: summ }}></article>
+            <a
+              className="Original"
+              rel="noreferrer"
+              target="_blank"
+              href={m.show.url}
+            >
+              Visit on TVMaze
+            </a>
           </div>
         );
       })}

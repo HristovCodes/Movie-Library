da1dad48815c60aa93f591a55c2e977bb90bc605
HristovCodes <ivailohristov1990@gmail.com>
2021-09-02 23:49:03 +0300

    gh-pages

--- src/store.tsx
+++
@@ -1,9 +0,0 @@
-import { createStore } from "@reduxjs/toolkit";
-import { moviesReducer } from "./reducers";
-
-export const store = createStore(moviesReducer);
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
